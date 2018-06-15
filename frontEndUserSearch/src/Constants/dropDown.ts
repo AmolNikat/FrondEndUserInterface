621bcaacb1b979b6fb8d2a3dc8a2cd5d4c295040
amol <amolnikat13@gmail.com>
2018-06-15 22:24:16 +0530

    fixed typo

--- frontEndUserSearch/src/Constants/dropDown.ts
+++ frontEndUserSearch/src/Constants/dropDown.ts
@@ -3,6 +3,6 @@
 export const dropDownList = [
     {'Key': '0', 'Value': 'Name (A-Z)'},
     {'Key': '1', 'Value': 'Name (Z-A)'},
-    {'Key': '2', 'Value': 'Rank (Aecending)'},
+    {'Key': '2', 'Value': 'Rank (Ascending)'},
     {'Key': '3', 'Value': 'Rank (Descending)'}
 ];
